45ad0babf31d04c04da57edb9d728cef0dca1c6e
temukaqveladze <Temokveladze127@gmail.com>
2024-05-22 20:56:06 +0400

    commit

---
+++ level 1/level 1.py
@@ -0,0 +1,38 @@
+name= "nTemuka"
+#name არის ცვლადი
+#=ცვლადისთვის მიმნიჭებელი სიმბოლო
+#"Temuka" არის ცვლადისთვის მნიშვნელობა
+
+sirname= "qveladze"
+
+print (name)
+#პრინტი ფუნქცია გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "Temuka" #es aris str (string) ტიპის ცვლადი
+age = 10 #ეს არის int ( integer ) მტელი რიცხვი
+
+#boolean "bool ტიპის ცვლადი"
+
+knows_programming= True   #True ან False
+is_ugly = False #cnakecnase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False  #ჯავასკრიპტული  camelcase
+
+#სტრინგი არის ბრჭყალებში მქცეული სიმბოლოები
+print(name+age)
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლო
+# print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print (name + " " + str(age))
+
+
+
