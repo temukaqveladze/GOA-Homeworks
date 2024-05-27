ed9dcdcd7a05b33f305eabe57c1c818f85f6ed99
temukaqveladze <Temokveladze127@gmail.com>
2024-05-27 22:26:45 +0400

    commit

---
+++ level3/level 3 - Copy.py
@@ -0,0 +1,35 @@
+#სახელი
+name= "Temuka"
+
+#გვარი
+sirname= 'qveladze'
+
+#წელი
+age= 10
+
+#მისამართი
+misamarti= "kumisi 27 Georgia"
+
+#საყვარელი საჭმელი
+namanama="pasta,borscht"
+
+#საყვარელი ფერი
+fori="blue"
+
+#საყვარელი მანქანა
+bugatata="Bugatti"
+
+print(name)
+print(misamarti)
+
+
+
+
+
+
+
+
+
+
+print(5 * 7)
+print("Temuka kveladze")
