250f2de7b9eab916ed3515c42cd159a3d1029f88
temukaqveladze <Temokveladze127@gmail.com>
2024-06-20 18:48:36 +0400

    commit

---
+++ axsna/trtr.py
@@ -0,0 +1,24 @@
+for i in range(20):
+    print("მე მიყვარს იმერული ხაჭაპური")
+
+#ახსნა 2
+
+for i in range(10):
+    print(i)
+
+
+#ახსნა 3
+
+# :-colon symbol  (ორი წერტილი ნიშნავს ჩამოთვლას)
+
+#While ლუპები ბეჭდავენ(print)იქამდე სანამ პირობა არის მართალი
+seats =100
+while seats > 0:
+    print('There are seats left!')
+    seats = seats - 1
+
+# ახსნა 4
+#counter ამბობს რომ არმდენჯერ უნდა დაპრინტოს
+
+
+
